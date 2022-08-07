361674b81bcc32db36457dab448988a920d1d015
Yurii <yuralukirin@gmail.com>
2022-08-07 16:20:48 +0300

    feat: added input and save input value to local storage

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const myTime = document.querySelector(".time");
 const myDate = document.querySelector(".date");
-
+const greet = document.querySelector(".greeting");
 
 
 function showTime() {
@@ -9,6 +9,7 @@ function showTime() {
     myTime.textContent = currentTime;
     setTimeout(showTime, 1000);
     showDate();
+    showGreeting();
 }
 
 showTime();
@@ -19,4 +20,37 @@ function showDate() {
     const currentDate = date.toLocaleDateString('de-De', options);
     myDate.textContent = currentDate;
 }
+function getTimeOfDay() {
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay = '';
+    if(hours > 0 && hours <= 6) {
+        timeOfDay = "night";
+    } else if (hours > 6 && hours <= 12) {
+        timeOfDay = "morning";
+    } else if(hours > 12 && hours <= 18) {
+        timeOfDay = "day";
+    } else if (hours > 18 && hours <= 24) {
+        timeOfDay = "evening";
+    }
+    return timeOfDay;
+}
+function showGreeting() {
+    const timeOfDay = getTimeOfDay();
+    greet.textContent = `Good ${timeOfDay}`;
+}
 
+
+let userName = document.querySelector(".name");
+
+function setLocalStorage() {
+    localStorage.setItem('name', userName.value);
+  }
+window.addEventListener('beforeunload', setLocalStorage)
+
+function getLocalStorage() {
+  if(localStorage.getItem('name')) {
+    userName.value = localStorage.getItem('name');
+  }
+}
+window.addEventListener('load', getLocalStorage);
